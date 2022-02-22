cab330d8d0324b002db10d933b815e0e7a89251d
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2022-02-22 21:49:43 +0000

    Fine esercizio sulle ore

--- app2.py
+++ app2.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 from datetime import datetime
 
-today = datetime.now()
-
-
+@app.route('/')
+def time():
+    now = datetime.now().hour
+    if now < 7:
+       return   render_template("index2.html", color="yellow",testo='È notte,sono le: ' + str(now))
+    elif now < 13:
+      return render_template("index2.html", color="red",testo= 'È mattino,sono le: ' +str(now))
+    elif now < 18:
+      return  render_template("index2.html", color="orange",testo= 'È pomeriggio,sono le: ' +str(now))
+    else:
+       return render_template("index2.html", color="blue",testo= 'È sera,sono le: ' + str(now))
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3001, debug=True)
