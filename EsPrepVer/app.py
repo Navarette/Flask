8b3e63308d3d8aa87fd7b1ef06f9c5f97cbb3c9d
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2022-03-23 13:53:57 +0000

    fine esercizio per la preparazione alla verifica

--- EsPrepVer/app.py
+++ EsPrepVer/app.py
@@ -11,8 +11,9 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-quartieri = gpd.read_file('/workspace/Flask/EsPrepVer/templates/ds964_nil_wm-20220322T104418Z-001 (1).zip')
-
+quartieri = gpd.read_file('/workspace/Flask/EsPrepVer/static/ds964_nil_wm-20220322T104418Z-001 (1).zip')
+fontanelle = gpd.read_file('/workspace/Flask/EsPrepVer/static/Fontanelle (1).zip')
+# 1
 @app.route('/', methods=['GET'])
 def home_page():
     return render_template('home.html')
@@ -29,7 +30,7 @@ def visualizza():
     FigureCanvas(fig).print_png(output)
 
     return Response(output.getvalue(), mimetype='image/png')
-
+#2
 @app.route('/quartiere', methods=['GET'])
 def home1_page():
 
@@ -53,22 +54,37 @@ def mpl():
     qrt = request.args['quartiere']
     imgUtente = quartieri[quartieri['NIL']==qrt]
 
-    return render_template('risultato.html',PageTitle = "Matplotlib")
+    return render_template('risultato.html',PageTitle = "Matplotlib",quartiere=qrt)
+#3
 
 @app.route('/scelta', methods=['GET'])
-def home2_page():
-
-    return render_template('scelta.html')
-
-@app.route('/quartieri.png', methods=['GET'])
 def scelta():
+    qrt = quartieri.NIL.to_list()
+    qrt.sort()
+    return render_template('scelta.html',PageTitle = "Matplotlib",quartieri=qrt)
+#4 
+@app.route("/fontanelle", methods=["GET"])
+def fontanelle1():
+    return render_template("fontanelle.html", quartieri= quartieri["NIL"])
 
+@app.route('/fontanelleres', methods=("POST", "GET"))
+def fontanelleRes():
+    global imgUtente, fontQuart
+    
+    quartiereUtente = request.args["Quartiere"]
+    imgUtente = quartieri[quartieri["NIL"] == quartiereUtente]
+    fontQuart = fontanelle[fontanelle.within(imgUtente.geometry.squeeze())]
+    print(fontQuart)
+    return render_template('fontanelleRes.html', PageTitle = "Matplotlib", quartiere=quartiereUtente, tabella = fontQuart.to_html())
+
+@app.route("/fontanelle.png", methods=["GET"])
+def Fontanelle():
     fig, ax = plt.subplots(figsize = (12,8))
-    qrt = request.args['quartieri']
-    df1 = quartieri[quartieri['NIL']==qrt]
 
-    df1.to_crs(epsg=3857).plot(ax=ax, alpha=0.5,edgecolor='k')
-    contextily.add_basemap(ax=ax)
+    imgUtente.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor="k")
+    fontQuart.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor="k")
+    contextily.add_basemap(ax=ax)   
+
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
