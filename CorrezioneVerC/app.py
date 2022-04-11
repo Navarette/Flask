90d7ed357f6ce1dcb56c600b7224ca7452844377
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2022-04-11 17:04:54 +0000

    fine correzione ver c

--- CorrezioneVerC/app.py
+++ CorrezioneVerC/app.py
@@ -10,10 +10,15 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
+# pip install flask geopandas matplotlib contextily pandas
 
 quartieri = gpd.read_file('/workspace/Flask/CorrezioneVerC/static/ds964_nil_wm-20220406T112003Z-001.zip')
-tram = gpd.read_file('/workspace/Flask/CorrezioneVerC/static/tpl_percorsi.geojson')
+linee = gpd.read_file('/workspace/Flask/CorrezioneVerC/static/tpl_percorsi.geojson')
 
+#trafosrma la colonna lung_km in valori float
+linee["lung_km"] = linee["lung_km"].astype(float) 
+#trasforma la colonna linea in valori integer
+linee["linea"] = linee["linea"].astype(int)
 @app.route("/", methods=["GET"])
 def home():
     return render_template("home.html")
@@ -32,68 +37,60 @@ def selezione():
 # ES 1
 @app.route("/numero", methods=["GET"])
 def numero():
-    return render_template('elenco.html')
+    return render_template('numero.html')
 
 @app.route("/ricerca", methods=["GET"])
 def ricerca():
-    global lunghezzautente
-    linea =  min(request.args["val1"], request.args["val2"])
-    linea1 =  max(request.args["val1"], request.args["val2"])
-    linee_distanza = tram[(tram["lung_km"] > linea) & (tram["lung_km"] < linea1)].sort_values("linea")
+    #trova il minimo tra il val1 e val2
+    linea =  min(float(request.args["val1"]), float(request.args["val2"]))
+    #trova il massimo tra il val1 e val2
+    linea1 = max(float(request.args["val1"]), float(request.args["val2"]))
+    #crea una variabile contenente i percorsi che si trovano tra i valori inseriti prima e li riordina in base alla colonna linea in ordine crescente
+    linee_distanza = linee[(linee["lung_km"] > linea) & (linee["lung_km"] < linea1)].sort_values("linea")
     return  render_template('risultato.html',risultato=linee_distanza.to_html())
 
 # ES 2
 @app.route("/input", methods=["GET"])
 def input():
-    return render_template('input.html')
+    return render_template("input.html")
 
-@app.route("/ricerca1", methods=["GET"])
-def ricerca1():
-    #dichiarazione globale di due variabili
-    global stazioniQuartiere ,quartiere
-    #controlla quello che l'utente ha inserito nella textbox
-    nomeQuar = request.args['quartiere']
-    #cerca nel dataframe quartieri il quartiere corrispondente al quartiere inserito dall'utente 
-    quartiere = quartieri[quartieri.NIL.str.contains(nomeQuar)]
-    #controlla i quartieri che intersecano con il geodataframe stazionigeo
-    tramQuar = tram[tram.intersects(quartiere.geometry.squeeze())]
-    
-    return render_template('elenco1.html',risultato = tramQuar.to_html())
+@app.route("/lineequart", methods=["GET"])
+def lineequart():
+    #controlla cosa l'utente ha inserito nella textbox
+    quartiere = request.args["quartiere"]
+    #controlla nella colonna 'NIL' il quartiere che corrisponde a quello che l'utente ha inserito
+    quartiereUtente = quartieri[quartieri["NIL"].str.contains(quartiere)]
+    #controlla nel dataframe 'linee' se qualcosa interseca con la geometria del quartiere inserito dall'utente
+    linee_quartiere = linee[linee.intersects(quartiereUtente.geometry.squeeze())].sort_values("linea")
+    return render_template("lineequart.html", tabella = linee_quartiere.to_html())
 
 # ES 3
 @app.route("/dropdown", methods=["GET"])
 def dropdown():
-    linea = tram.linea.to_list()
-     
-    linea = list(set(linea))
-    
-    linea.sort()
-
-    return render_template('dropdown.html',linee=linea)
+                                           #crea una variabile contenente la colonna linea droppando i duplicati e mettendo in ordine crescente 
+    return render_template('dropdown.html',linee = linee["linea"].drop_duplicates().sort_values(ascending=True))
 
 @app.route("/sceltalinea", methods=["GET"])
-def sceltastazione():
-    global quartiere1,lineaUtente
-   
-    linea = request.args['linea']  
-    
-    lineaUtente = tram[tram.linea== linea] 
+def sceltastazione(): 
+    global lineeUtente
+    #controlla quello che l'utente sceglie nel menu a tendina 
+    linea = int(request.args["linea"])
+    #cerca nel geodataframe 'linee' nella colonna 'linea' quello che corrisponde a quello che l'utente ha selezionato
+    lineeUtente = linee[linee["linea"] == linea]
 
-    quartiere1 = quartieri[quartieri.contains(lineaUtente.geometry.squeeze())]
-
-    return render_template('sceltalinea.html')
+    return render_template('sceltalinea.html',linea = linea)
 
 @app.route("/mappaquar", methods=["GET"])
 def mappaquar():
+
     fig, ax = plt.subplots(figsize = (12,8))
 
-    lineaUtente.to_crs(epsg=3857).plot(ax=ax,color='black')
-    quartiere1.to_crs(epsg=3857).plot(ax=ax,alpha=0.3,edgecolor='k')
-    contextily.add_basemap(ax=ax)
+    lineeUtente.to_crs(epsg=3857).plot(ax=ax, edgecolor="k")
+    quartieri.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor="k")
+    contextily.add_basemap(ax=ax)   
+
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
-    FigureCanvas(fig).print_png(output)
-    
     return Response(output.getvalue(), mimetype='image/png')
 
 
